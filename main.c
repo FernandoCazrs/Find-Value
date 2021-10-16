7cdd0032172de40318419d36ba3968c45d0a6b6d
FernandoCazrs <2173071926@cua.uam.mx>
2021-10-15 20:01:06 -0500

    Inicio

--- main.c
+++ main.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+#define arr_size 10
+int A[arr_size] = { 1, 4, 45, 6, 10, 8, 6,24,30, 2};
+
 
 // returns true if there is triplet with sum equal
-// to 'sum' present in A[]. 
-bool find3Numbers(int A[], int arr_size, int sum)
+// to 'sum' present in A[]. Also, prints the triplet
+bool find3Numbers(int A[], int sum)
 {
 	int l, r;
 
 	// Fix the first element as A[i]
-	for (int i = 0; i < arr_size -2 ; i++) {
-		
+	for (int i = 0; i < arr_size - 2; i++) {
+
 		// Fix the second element as A[j]
 		for (int j = i + 1; j < arr_size - 1; j++) {
 
 			// Now look for the third number
 			for (int k = j + 1; k < arr_size; k++) {
-				if (A[i] + A[j] + A[k] == sum) {
+				if (A[i] + A[j] + A[k] == sum) {					
 					return true;
 				}
 			}
@@ -28,9 +33,7 @@ bool find3Numbers(int A[], int arr_size, int sum)
 /* Driver program to test above function */
 int main()
 {
-	int A[] = { 1, 4, 45, 6, 10, 8 , 4, 20, 43, 20};
 	int sum = 22;
-	int arr_size = 10;
-	find3Numbers(A, arr_size, sum);
+	find3Numbers(A,  sum);
 	return 0;
 }
